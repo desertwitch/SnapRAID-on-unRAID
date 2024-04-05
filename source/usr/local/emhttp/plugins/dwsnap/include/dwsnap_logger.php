c64496f8220f8cae3134ff2ba0c1ce107c1185f6
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-05 13:15:08 +0200

    Update dwsnap_logger.php

--- source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_logger.php
+++ source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_logger.php
@@ -4,9 +4,9 @@ if(file_exists("/var/log/snapraid/snaplog")) {
     if(!empty($snaplog)) {
         echo("<pre class='snaplog'>".$snaplog."</pre>");
     } else {
-        echo("<pre class='snaplog'>LOG FILE FOUND BUT IS EMPTY</pre>");
+        echo("<pre class='snaplog'></pre>");
     }
 } else {
-    echo("<pre class='snaplog'>NO LOG FILE FOUND</pre>");
+    echo("<pre class='snaplog'></pre>");
 }
 ?>
