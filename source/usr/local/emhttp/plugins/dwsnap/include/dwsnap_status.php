96a05fd75adbbefabd38d6734cc1f2c3dc6a7d0c
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-05 12:34:05 +0200

    changes

--- source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_status.php
+++ source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_status.php
@@ -1,4 +1,4 @@
 <?
-$snapraid_running = (intval(trim(htmlspecialchars(shell_exec( "if pgrep -x snaprunner >/dev/null 2>&1; then echo 1; else echo 0; fi" )))) === 1 );
+$snapraid_running = trim(htmlspecialchars(shell_exec( "if pgrep -x snaprunner >/dev/null 2>&1; then echo YES; else echo NO; fi" )));
 echo("RUNNING:".$snapraid_running);
 ?>
