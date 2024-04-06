23603fad5dd2c0ce899f31b524f0903528056db7
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-06 08:25:42 +0200

    changes

--- source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_config.php
+++ source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_config.php
@@ -1,6 +1,6 @@
 <?
 $dwsnap_cfg  = parse_ini_file("/boot/config/plugins/dwsnap/dwsnap.cfg");
-$dwsnap_prio = trim(isset($dwdvm_cfg['PRIO']) ? htmlspecialchars($dwdvm_cfg['PRIO']) : 'disable');
+$dwsnap_prio = trim(isset($dwsnap_cfg['PRIO']) ? htmlspecialchars($dwsnap_cfg['PRIO']) : 'disable');
 
 $snapraid_backend = trim(shell_exec("find /var/log/packages/ -type f -iname 'snapraid-*' -printf '%f\n' 2> /dev/null"));
 $snapraid_laststart = (file_exists("/var/log/snapraid/laststart") ? (file_get_contents("/var/log/snapraid/laststart") ?? "n/a") : "n/a");
