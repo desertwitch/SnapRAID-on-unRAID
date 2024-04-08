6436ff604765a26e42787fe4af3394e5f778b60e
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-08 10:03:16 +0200

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -3,6 +3,7 @@ BOOT="/boot/config/plugins/dwsnap"
 DOCROOT="/usr/local/emhttp/plugins/dwsnap"
 
 chmod +0755 $DOCROOT/scripts/*
+chmod +0755 /usr/bin/snapraid-cron
 chmod +0755 /usr/bin/snapraid-runner
 
 if [ ! -d $BOOT/config ]; then
