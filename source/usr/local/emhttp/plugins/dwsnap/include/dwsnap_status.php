20295f5ffac74047153be83c5894b84328d7c14e
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-11 18:27:42 +0200

    changes

--- source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_status.php
+++ source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_status.php
@@ -1,4 +1,4 @@
 <?
-$snap_running = trim(htmlspecialchars(shell_exec( "if pgrep -x snapraid >/dev/null 2>&1 || pgrep -x snapraid-cron >/dev/null 2>&1 || pgrep -x snapraid-runner >/dev/null 2>&1; then echo YES; else echo NO; fi" )));
+$snap_running = trim(htmlspecialchars(shell_exec( "if pgrep -x snapraid >/dev/null 2>&1 || pgrep -x snapraid-cron >/dev/null 2>&1 || pgrep -x snapraid-runner >/dev/null 2>&1; then echo YES; else echo NO; fi" ) ?? "-"));
 echo("RUNNING:".$snap_running);
 ?>
