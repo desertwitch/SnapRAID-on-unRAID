359baf7d0957ad7007a905c89ffe424427608b35
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-04-26 23:32:22 +0200

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -25,10 +25,12 @@ if [ ! -d $BOOT/config ]; then
     mkdir -p $BOOT/config
 fi
 
-if ! mountpoint -q /var/lib/snapraid 2>/dev/null; then 
+if ! mountpoint -q /var/lib/snapraid; then 
     rm -rf /var/lib/snapraid
     mkdir -p /var/lib/snapraid
-    mount -t tmpfs -o size=30% tmpfs /var/lib/snapraid
+    if ! mount -t tmpfs -o size=30% tmpfs /var/lib/snapraid; then
+        echo "(ERROR) Failed to create a RAM disk for SnapRAID... falling back to a regular folder!" | logger -t "snapraid-installer"
+    fi
 fi
 
 if [ ! -d /var/lib/snapraid/logs ]; then
