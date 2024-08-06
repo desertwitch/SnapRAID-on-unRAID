d92e3fb936a67070f06673f220504bc5d1d4520e
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-08-06 07:07:34 +0200

    identation

--- source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_download.php
+++ source/usr/local/emhttp/plugins/dwsnap/include/dwsnap_download.php
@@ -19,25 +19,25 @@
  */
 exec("timeout -s9 30 /usr/local/emhttp/plugins/dwsnap/scripts/ziplogs", $snap_debug_result);
 if(!empty($snap_debug_result)) {
-	if(strpos(end($snap_debug_result), "DONE:") !== false) {
-		$snap_debug_file = explode(":", end($snap_debug_result))[1];
-		if(!empty($snap_debug_file) && file_exists($snap_debug_file)) {
-			header("Content-Disposition: attachment; filename=\"" . basename($snap_debug_file) . "\"");
-			header("Content-Type: application/octet-stream");
-			header("Content-Length: " . filesize($snap_debug_file));
-			header("Connection: close");
-			readfile($snap_debug_file);
-			unlink($snap_debug_file);
-			exit;
-		} else {
-			echo("ERROR: The log package generation script has failed - bash backend returned filename, PHP could not find file.");
-		}
-	} else { 
-		echo("ERROR: The log package generation script has failed - response from the bash backend:<br><pre>");
-		echo(implode("\n",$snap_debug_result));
-		echo("</pre>");
-	}
+    if(strpos(end($snap_debug_result), "DONE:") !== false) {
+        $snap_debug_file = explode(":", end($snap_debug_result))[1];
+        if(!empty($snap_debug_file) && file_exists($snap_debug_file)) {
+            header("Content-Disposition: attachment; filename=\"" . basename($snap_debug_file) . "\"");
+            header("Content-Type: application/octet-stream");
+            header("Content-Length: " . filesize($snap_debug_file));
+            header("Connection: close");
+            readfile($snap_debug_file);
+            unlink($snap_debug_file);
+            exit;
+        } else {
+            echo("ERROR: The log package generation script has failed - bash backend returned filename, PHP could not find file.");
+        }
+    } else { 
+        echo("ERROR: The log package generation script has failed - response from the bash backend:<br><pre>");
+        echo(implode("\n",$snap_debug_result));
+        echo("</pre>");
+    }
 } else {
-	echo("ERROR: The log package generation script has failed - no response from the bash backend.");
+    echo("ERROR: The log package generation script has failed - no response from the bash backend.");
 }
 ?>
